66aadb60952b2a17ffd93e94de73a63d013287ba
CZrich <czrichardz@gmail.com>
2024-06-25 23:29:25 -0500

    clipboard,encriptar y desencriptar implementados

--- app.js
+++ app.js
@@ -1,80 +1,105 @@
-let hola ="hola mundo";
-function encritador(inputmgs="gente como van"){
-   let mensage=inputmgs.split('');
+let hola = "hola mundo";
 
-    for(let i=0;i<mensage.length;i++){
-    
-    
-     
-        
-        switch(mensage[i]){
-            case "e"://enter
-             mensage
-             break;
-             case "i"://imes
-             mensage[i]="imes";
-             break;
-             case "a"://ai
-             mensage[i]="ai";
-             break;
-             case "o"://ober
-             mensage[i]="ober";
-             break;
-             case "u"://ufat
-             mensage[i]="ufat";
-             break;
-        }
-    }
- 
-
-   console.log(mensage.join(''));
+function limpiarInput(id) {
+  document.getElementById(id).value = "";
 }
-encritador(hola);
-
-function desencriptar( msg="asfafa"){
-    let mensage=msg.split('');
-      //console.log("arr in des;",mensage);
-    /* msg= msg.replaceAll("enter","e");
-     msg =msg.replaceAll("imes","i");
-     msg =msg.replaceAll("ai","a");
-
-     msg =msg.replaceAll("ober","o");
-     msg =msg.replaceAll("ufat","u");
- */
-
-   
-  for(let i=0;i<mensage.length;i++){
-    
-    
-     
-        
-        switch(mensage[i]){
-            case "e"://enter
-             //mensage[i]="e";
-              mensage.splice(i+1,4);
-             break;
-             case "i"://imes
-             mensage.splice(i+1,3);
-             break;
-             case "a"://ai
-             mensage.splice(i+1,1);
-             break;
-             case "o"://ober
-             //mensage[i]="o";
-             mensage.splice(i+1,3);
-             break;
-             case "u"://ufat
-             mensage.splice(i+1,3);
-             break;
-        }
-    }
- 
-
-
-
-  console.log(mensage.join(''));
-  //console.log(msg);
+function estadoVisibleElemento(id, estado) {
+  document.getElementById(id).style.visibility = estado;
 }
-let mensage="fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!"
-//desencriptar("hoberlai mufatndober");
-desencriptar(mensage);
+function estadoRenderizarElemento(id, estado) {
+  document.getElementById(id).style.display = estado;
+}
+function optenerInput(id) {
+  return document.getElementById(id).value;
+}
+function setTextToHTML(id, texto) {
+  document.getElementById(id).innerText = texto;
+}
+function encriptarMensaje() {
+  let mensage = optenerInput('inputTexto');
+  mensage = encritador(mensage);
+  limpiarInput('inputTexto');
+
+  setTextToHTML('parrafo__encriptado', mensage);
+  estadoRenderizarElemento('imagen', 'none');
+  estadoVisibleElemento('bonton__copiar', 'visible');
+}
+
+function desencriptarMensaje() {
+  let mensage = optenerInput('inputTexto');
+  limpiarInput('inputTexto');
+  mensage = desencriptar(mensage);
+  setTextToHTML('parrafo__encriptado', mensage);
+}
+function copiar(){
+ 
+    // Obtener el contenido del elemento
+    var content = document.getElementById("parrafo__encriptado").innerText;
+  
+    // Usar la API de Portapapeles para copiar el contenido
+    navigator.clipboard.writeText(content).then(function() {
+      console.log("first")
+    }).catch(function(error) {
+      alert("Error al copiar el contenido: " + error);
+    });
+  
+}
+
+function encritador(inputmgs) {
+
+  let mensage = inputmgs.split('');
+
+  for (let i = 0; i < mensage.length; i++) {
+    switch (mensage[i]) {
+      case "e"://enter
+        mensage
+        break;
+      case "i"://imes
+        mensage[i] = "imes";
+        break;
+      case "a"://ai
+        mensage[i] = "ai";
+        break;
+      case "o"://ober
+        mensage[i] = "ober";
+        break;
+      case "u"://ufat
+        mensage[i] = "ufat";
+        break;
+    }
+  }
+
+  return mensage.join('');
+
+}
+
+
+function desencriptar(msg) {
+  let mensage = msg.split('');
+
+  for (let i = 0; i < mensage.length; i++) {
+
+    switch (mensage[i]) {
+      case "e"://enter
+        //mensage[i]="e";
+        mensage.splice(i + 1, 4);
+        break;
+      case "i"://imes
+        mensage.splice(i + 1, 3);
+        break;
+      case "a"://ai
+        mensage.splice(i + 1, 1);
+        break;
+      case "o"://ober
+        //mensage[i]="o";
+        mensage.splice(i + 1, 3);
+        break;
+      case "u"://ufat
+        mensage.splice(i + 1, 3);
+        break;
+    }
+  }
+
+  return mensage.join('');
+
+}
